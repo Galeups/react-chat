c7bb19f4b3d64d4d7358047b5891d63d75bf2c5b
Galeups <artdevice@gmail.com>
2020-08-18 23:02:44 +0200

    Add a router and a secure guard route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import './App.scss';
-import {
-  BrowserRouter,
-  Switch,
-  Router,
-  Redirect,
-  useLocation
-} from "react-router-dom";
+import React from "react";
+import "./App.scss";
+import { Redirect, Route, Router, Switch, useLocation } from "react-router-dom";
 
+import AddRoom from "./components/AddRoom/AddRoom";
+import Login from "./components/Login/Login";
+import RoomList from "./components/RoomList/RoomList";
+import ChatRoom from "./components/ChatRoom/ChatRoom";
 
 function App() {
+  let location = useLocation();
+
+  const SecureRoute = ({ children, ...rest }) => (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        localStorage.getItem("nickname") ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to my <b>React Chat</b> application.
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Redirect
+          to={{
+            pathname: "roomlist",
+            state: { from: location },
+          }}
+        />
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <SecureRoute path="/roomlist">
+            <RoomList />
+          </SecureRoute>
+          <SecureRoute path="/addroom">
+            <AddRoom />
+          </SecureRoute>
+          <SecureRoute path="/chatroom/:room">
+            <ChatRoom />
+          </SecureRoute>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
